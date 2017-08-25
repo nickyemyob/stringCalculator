e780291eb965dd526151be6c11462791f8b4111d
Karen Xie <karen.xie@myob.com>
2017-08-25 15:24:17 +1200

    create add method

---
+++ StringCalculator/StringCalculatorShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator
+{
+    [TestClass]
+    public class StringCalculatorShould
+    {
+        [TestMethod]
+        public void Return0WithEmptyString()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+
+            var result = stringCalculator.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
