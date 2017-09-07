870b12518c09983eef753dd604a25ebe28521a3c
Nick Ye <30612557+nickyemyob@users.noreply.github.com>
2017-09-07 22:52:12 +1200

    Created StringToListParser class, moved methods from stringCalculator to StringToListParser and refactored

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StringCalculator
 {
     public class StringCalculator
     {
-        private const char DelimitersAndNumbersSeparator = '\n';
-        private const string DelimiterHeader = "//";
-        private const string ErrorMessage = "Negatives not allowed:";
-        private static readonly char[] DefaultDelimiters = {',', '\n'};
+
+        private readonly StringToListParser _stringToListParser;
+
+        public StringCalculator()
+        {
+            _stringToListParser = new StringToListParser();
+        }
 
         public int Add(string input)
         {
@@ -22,7 +22,7 @@ namespace StringCalculator
                 return count;
             }
 
-            var numbersAsStringList = ExtractNumbersToList(input);
+            var numbersAsStringList = _stringToListParser.ExtractNumbersToList(input);
             var numbersAsIntList = numbersAsStringList.Select(int.Parse).ToList();
             var negativeNumbers = new List<int>();
 
@@ -45,95 +45,9 @@ namespace StringCalculator
                 return count;
             }
 
-            var errorMessage = BuildErrorMessage(negativeNumbers);
+            var errorMessage = _stringToListParser.BuildErrorMessage(negativeNumbers);
             throw new ArgumentException(errorMessage);
 
-        }
-
-        private IEnumerable<string> ExtractNumbersToList(string numbers)
-        {
-            if (!ContainsCustomDelimiter(numbers))
-            {
-                return new List<string>(numbers.Split(DefaultDelimiters));
-            }
-
-            var delimitersAndNumbers = new List<string>(numbers.Split(DelimitersAndNumbersSeparator));
-            var delimiter = ExtractDelimiter(delimitersAndNumbers[0]);
-
-            return ExtractNumbers(delimitersAndNumbers, delimiter);
-        }
-
-        private string[] ExtractDelimiter(string stringContainingDelimiter)
-        {
-            var delimiter = stringContainingDelimiter.Replace(DelimiterHeader, "");
-            if (delimiter.Equals(""))
-            {
-                return new [] { "\n" };
-            }
-            var firstBracketIndex = delimiter.IndexOf("[", StringComparison.Ordinal);
-            var lastBracketIndex = delimiter.LastIndexOf("]", StringComparison.Ordinal);
-            if (firstBracketIndex <= -1 || lastBracketIndex <= -1)
-            {
-                return new[] {delimiter};
-            }
-
-            return ExtractMultipleDelimiters(delimiter);
-           
-        }
-
-        private static string[] ExtractMultipleDelimiters(string multipleDelimiters)
-        {
-            var tempDelimiter = "";
-            var delimiters = new List<string>();
-            foreach (var str in multipleDelimiters)
-            {
-                if (str.Equals('['))
-                {
-                }
-                else if (str.Equals(']'))
-                {
-                    delimiters.Add(tempDelimiter);
-                    tempDelimiter = "";
-                }
-                else
-                {
-                    tempDelimiter += str;
-                }
-            }
-            return delimiters.ToArray();
-        }
-
-        private static IEnumerable<string> ExtractNumbers(IList<string> delimitersAndNumbers, string[] delimiter)
-        {
-             if (!delimiter.Contains("\n"))
-            {
-                return new List<string>(delimitersAndNumbers[1].Split(delimiter, StringSplitOptions.None));
-            }
-
-            return RemoveNonNumbers(delimitersAndNumbers);
-        }
-
-        private static IEnumerable<string> RemoveNonNumbers(IList<string> delimitersAndNumbers)
-        {
-            delimitersAndNumbers.RemoveAt(0);
-            delimitersAndNumbers.RemoveAt(0);
-            return delimitersAndNumbers;
-        }
- 
-
-        private static bool ContainsCustomDelimiter(string input)
-        {
-            return input.StartsWith(DelimiterHeader);
-        }
-
-        private string BuildErrorMessage(IEnumerable<int> negativeNumbers)
-        {
-            var errorMessage = ErrorMessage;
-            foreach (var number in negativeNumbers)
-            {
-                errorMessage += " " + number;
-            }
-            return errorMessage;
         }
     }
 }
